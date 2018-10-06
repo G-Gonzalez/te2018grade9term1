4f2fd5e49ab931ffcef41ccc03d39dfbe80cb9aa
Greily <greily.gonzalez@csedge.org>
2018-10-06 15:45:42 -0400

    Completed Challenge 1

--- data.js
+++ data.js
@@ -1,5 +1,26 @@
-// Example
-let message = "Hello World!";
-console.log(message);
-
+const READLINE = require("readline-sync");
 // Type your code below!
+//String
+let seo = "-SEO Scholar";
+console.log(typeof seo);
+let college = "-MIT";
+console.log(college);
+let degree = "-Masters in software engineering and mechanical engineering"; 
+console.log (degree);
+//Number
+let month = 09;
+let day = 26;
+let year = 2004;
+console.log (typeof month); 
+console.log (-day); 
+console.log(-year);
+//Boolean
+let dominican = true;
+let french = false;
+console.log(typeof dominican);
+console.log(-french);
+//Undefined
+let music;
+console.log(music);
+let userInput = READLINE.question("Type anything and I'll repeat it back");
+console.log(userInput);
